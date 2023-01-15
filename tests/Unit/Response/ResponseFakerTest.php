bc8683df3b8799ac6922e32f8dd1706114d3ea14
Carsten Schindler <cschindl@gmx.de>
2023-01-15 14:04:07 +0100

    WIP add tests

--- tests/Unit/Response/ResponseFakerTest.php
+++ tests/Unit/Response/ResponseFakerTest.php
@@ -6,7 +6,9 @@ namespace Cschindl\OpenAPIMock\Tests\Unit\Response;
 
 use cebe\openapi\Reader;
 use cebe\openapi\spec\OpenApi;
+use Cschindl\OpenAPIMock\Exception\ValidationException;
 use Cschindl\OpenAPIMock\Response\ResponseFaker;
+use Exception;
 use League\OpenAPIValidation\PSR7\OperationAddress;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
@@ -16,6 +18,7 @@ use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\StreamInterface;
+use Vural\OpenAPIFaker\Exception\NoExample;
 use Vural\OpenAPIFaker\Exception\NoPath;
 use Vural\OpenAPIFaker\Exception\NoResponse;
 use Vural\OpenAPIFaker\Options;
@@ -47,6 +50,10 @@ paths:
                   properties:
                     id:
                       type: integer
+              examples: 
+                testExample:
+                  value:
+                    id: 100
 YAML;
         $this->schema = Reader::readFromYaml($yaml);
 
@@ -62,7 +69,7 @@ YAML;
         $this->streamFactory->createStream(Argument::any())->willReturn($stream);
     }
 
-    public function testMockPossibleResponseWithExistingStatusCode(): void
+    public function testMockWithStatusCode(): void
     {
         $operationAddress = new OperationAddress('/pet', 'GET');
 
@@ -77,7 +84,7 @@ YAML;
         self::assertInstanceOf(ResponseInterface::class, $response);
     }
 
-    public function testMockPossibleResponseWithMultipleStatusCodes(): void
+    public function testMockWithMultipleStatusCodes(): void
     {
         $operationAddress = new OperationAddress('/pet', 'GET');
 
@@ -92,7 +99,7 @@ YAML;
         self::assertInstanceOf(ResponseInterface::class, $response);
     }
 
-    public function testMockPossibleResponseWithNoResponse(): void
+    public function testMockWithNoResponse(): void
     {
         $operationAddress = new OperationAddress('/pet', 'GET');
 
@@ -107,7 +114,7 @@ YAML;
         $responseFaker->mock($this->schema, $operationAddress, '400', 'application/json');
     }
 
-    public function testMockPossibleResponseWithNoPath(): void
+    public function testMockWithNoPath(): void
     {
         $operationAddress = new OperationAddress('/', 'GET');
 
@@ -120,5 +127,76 @@ YAML;
         $this->expectException(NoPath::class);
 
         $responseFaker->mock($this->schema, $operationAddress, '200', 'application/json');
+    }
+
+    public function testMockWithNoExample(): void
+    {
+        $operationAddress = new OperationAddress('/pet', 'GET');
+
+        $responseFaker = new ResponseFaker(
+            $this->responseFactory->reveal(),
+            $this->streamFactory->reveal(),
+            ['strategy' => Options::STRATEGY_STATIC]
+        );
+
+        $this->expectException(NoExample::class);
+
+        $responseFaker->mock($this->schema, $operationAddress, '200', 'application/json', 'wrongExample');
+    }
+
+    public function testHandleExceptionWithRequestException(): void
+    {
+        $stream = $this->prophesize(StreamInterface::class);
+        $response = $this->prophesize(ResponseInterface::class);
+        $response->withBody($stream)->willReturn($response);
+        $response->withAddedHeader('Content-Type', 'application/json')->willReturn($response);
+        $response->withStatus(404)->willReturn($response);
+        $this->responseFactory->createResponse()->willReturn($response);
+
+        $body = [
+            'type' => 'NOT_FOUND',
+            'title' => 'The server cannot find the requested content',
+            'detail' => '',
+            'status' => 404,
+        ];
+        $this->streamFactory->createStream(json_encode($body))->willReturn($stream);
+
+        $responseFaker = new ResponseFaker(
+            $this->responseFactory->reveal(),
+            $this->streamFactory->reveal(),
+            ['strategy' => Options::STRATEGY_STATIC]
+        );
+
+        $response = $responseFaker->handleException(ValidationException::forNotFound(), 'application/json');
+
+        self::assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    public function testHandleExceptionWithUnexpectedException(): void
+    {
+        $stream = $this->prophesize(StreamInterface::class);
+        $response = $this->prophesize(ResponseInterface::class);
+        $response->withBody($stream)->willReturn($response);
+        $response->withAddedHeader('Content-Type', 'application/json')->willReturn($response);
+        $response->withStatus(500)->willReturn($response);
+        $this->responseFactory->createResponse()->willReturn($response);
+
+        $body = [
+            'type' => 'ERROR',
+            'title' => 'Unexpected error occurred',
+            'detail' => 'Unexpected Error',
+            'status' => 500,
+        ];
+        $this->streamFactory->createStream(json_encode($body))->willReturn($stream);
+
+        $responseFaker = new ResponseFaker(
+            $this->responseFactory->reveal(),
+            $this->streamFactory->reveal(),
+            ['strategy' => Options::STRATEGY_STATIC]
+        );
+
+        $response = $responseFaker->handleException(new Exception('Unexpected Error'), 'application/json');
+
+        self::assertInstanceOf(ResponseInterface::class, $response);
     }
 }
