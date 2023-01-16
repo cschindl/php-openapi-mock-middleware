6a82802c99fc225ce9bdc384ba672a2fa1ff270e
Carsten Schindler <cschindl@gmx.de>
2023-01-16 20:04:20 +0000

    Fix tests

--- tests/Integration/E2ETest.php
+++ tests/Integration/E2ETest.php
@@ -15,7 +15,6 @@ use League\OpenAPIValidation\PSR7\ValidatorBuilder;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7\Uri;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -28,26 +27,26 @@ class E2ETest extends TestCase
 
     private const SPECS = [
         'petstore',
-        // 'twitter',
+        'twitter',
         'uber',
-        // 'uspto',
+        'uspto',
     ];
 
     /**
      * @dataProvider provideValidRequest
      */
-    function testHandleValidRequest(ServerRequestInterface $request, string $yaml)
+    function testHandleValidRequest(ServerRequestInterface $request, int $expectedStatusCode, string $yaml)
     {
         $middleware = $this->createMiddleware($yaml);
 
         $handler = $this->prophesize(RequestHandlerInterface::class);
         $response = $middleware->process($request, $handler->reveal());
 
-        if ($response->getStatusCode() !== 200) {
+        if ($response->getStatusCode() !== $expectedStatusCode) {
             self::fail($response->getBody()->__toString());
         }
 
-        self::assertEquals('200', $response->getStatusCode());
+        self::assertEquals((string) $expectedStatusCode, $response->getStatusCode());
     }
 
     /**
@@ -76,18 +75,21 @@ class E2ETest extends TestCase
                     }
 
                     $request = $this->prophesize(ServerRequestInterface::class);
+                    $request->getHeader(OpenApiMockMiddleware::HEADER_FAKER_ACTIVE)->willReturn(['true']);
                     $request->getMethod()->willReturn($method);
 
                     $path = str_replace('{scheme}', 'https', $schema->servers[0]->url . $path);
                     $queryParams = [];
-                    $headers = [];
                     $cookieParams = [];
                     foreach ($operation->parameters as $parameter) {
                         if ($parameter->required === true) {
                             $fakeData = (new SchemaFaker($parameter->schema, (new Options())->setStrategy(Options::STRATEGY_STATIC)))->generate();
+                            if (is_array($fakeData)) {
+                                $fakeData = implode(',', $fakeData);
+                            }
 
                             if ($parameter->in === 'path') {
-                                $path = str_replace('{' . $parameter->name . '}', $fakeData, $path);
+                                $path = str_replace('{' . $parameter->name . '}', (string) $fakeData, $path);
                             }
 
                             if ($parameter->in === 'query') {
@@ -95,7 +97,7 @@ class E2ETest extends TestCase
                             }
 
                             if ($parameter->in === 'header') {
-                                $headers[$parameter->name][0] = $parameter->example;
+                                $request->getHeader($parameter->name)->willReturn([$parameter->example]);
                             }
 
                             if ($parameter->in === 'cookie') {
@@ -105,13 +107,15 @@ class E2ETest extends TestCase
                     }
 
                     $request->getQueryParams()->willReturn($queryParams);
-                    $request->getHeader(Argument::any())->willReturn($headers);
-                    $request->getHeader(OpenApiMockMiddleware::HEADER_FAKER_ACTIVE)->willReturn(['true']);
                     $request->getCookieParams()->willReturn($cookieParams);
                     $request->getUri()->willReturn(new Uri($path));
 
                     foreach ($operation->responses as $statusCode => $response) {
-                        if ($statusCode == 'default') {
+                        if ($statusCode === 'default') {
+                            continue;
+                        }
+
+                        if ($statusCode !== 200) {
                             continue;
                         }
 
@@ -122,10 +126,15 @@ class E2ETest extends TestCase
                                 continue;
                             }
 
+                            if ($contentType !== 'application/json') {
+                                continue;
+                            }
+
                             $request->getHeader(OpenApiMockMiddleware::HEADER_CONTENT_TYPE)->willReturn([$contentType]);
 
                             $data[$method . ':' . $path] = [
                                 $request->reveal(),
+                                $statusCode,
                                 $yaml
                             ];
                         }
@@ -142,7 +151,7 @@ class E2ETest extends TestCase
         $validatorBuilder = (new ValidatorBuilder())->fromYaml($yaml);
         $psr17Factory = new Psr17Factory();
         $settings = [
-            'minItems' => 5,
+            'minItems' => 1,
             'maxItems' => 10,
             'alwaysFakeOptionals' => true,
             'strategy' => Options::STRATEGY_STATIC,
